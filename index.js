cdcd2d62ca4391d048f5b88738541cb97b9de642
kevin breid <kevinbreid@yahoo.com>
2022-05-28 13:44:55 -0700

    commit tree e0c80e24e4e2fa056aff037c5917b58d9837688a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+
+const input = document.getElementById('button');
+
+function addingEventListener() {function clickAlert() {
+    alert('I was clicked!');
+  }
 }
+
+
+
+input.addEventListener();
